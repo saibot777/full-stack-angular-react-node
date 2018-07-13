66dd9fd0f5855ce2ed0efbabe73a17b48b433abb
saibot777 <stefantrajkovic777@gmail.com>
2018-07-13 18:54:49 +0200

    rental section update

--- client-ng/src/app/app.module.ts
+++ client-ng/src/app/app.module.ts
@@ -3,15 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from './common/header/header.component';
+import { RentalComponent } from './rental/rental.component';
+import {RouterModule, Routes} from '@angular/router';
+import { RentalListComponent } from './rental/rental-list/rental-list.component';
+import { RentalListItemComponent } from './rental/rental-list-item/rental-list-item.component';
 
+const routes: Routes = [
+  {path: '', component: RentalComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RentalComponent,
+    RentalListComponent,
+    RentalListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
