bbd2a9e49d291f50da6480f4b7b446fe61ab2982
saibot777 <stefantrajkovic777@gmail.com>
2018-07-13 21:50:03 +0200

    routing fix

--- client-ng/src/app/app-routing.module.ts
+++ client-ng/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './rental/rental.module#RentalModule' }
+  { path: '', redirectTo: '/rental', pathMatch: 'full' },
+  { path: 'rental', loadChildren: './rental/rental.module#RentalModule' }
 ];
 
 @NgModule({
